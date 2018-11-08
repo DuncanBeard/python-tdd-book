71213187b6495053c0a39af49ade0e1f4562aa94
Duncan Beard <dbeard@mash.localdomain>
2018-11-08 11:31:21 -0500

    Fleshed out FT for empty list item check

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -10,6 +10,7 @@ from unittest import skip
 
 MAX_WAIT = 10
 
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -28,6 +29,16 @@ class FunctionalTest(StaticLiveServerTestCase):
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
